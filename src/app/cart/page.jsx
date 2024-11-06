c7ed762b17844ca51837c4a9ef1eaa10f0be6536
Ashkan Golzad <ashkan72golzad@gmail.com>
2024-11-06 17:51:21 +0330

    final

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { useSelector, useDispatch } from 'react-redux';
 import { incrementQuantity,decrementQuantity,removeFromCart } from '../components/redux/cartSlice';
+import { Equal, Minus, Plus, X } from 'lucide-react';
 
 const Page = () => {
     const cart = useSelector((state) => state.cart);
@@ -16,21 +17,36 @@ const Page = () => {
     const TotalPrice = getTotalPrice().toFixed(2);
 
     return(
-        <div>
-            {cart.map((item) => (
-                <div>
-                <div>{item.title}</div> 
-                <button className='rounded shadow-lg bg-slate-100 px-3 py-1 hover:bg-slate-400 mr-1' onClick={() => dispatch(incrementQuantity(item.id))}>
-              +
-            </button>
-            <button className='rounded shadow-lg bg-slate-100 px-3 py-1 hover:bg-slate-400 mr-1' onClick={() => dispatch(decrementQuantity(item.id))}>
-              -
-            </button>
-            <button className='rounded shadow-lg bg-slate-100 px-3 py-1 hover:bg-slate-400' onClick={() => dispatch(removeFromCart(item.id))}>
-              x
-            </button>   
+        <div className='mt-16 md:w-[50%] md:ml-[25%] w-[95%] ml-[2.5%]'>
+            {cart.length == 0 ? <div className='w-full mt-16 mb-48 text-xl animate-pulse text-center font-["font2"]'>Your Cart Is Empty!</div> :
+            cart.map((item) => (
+                <div key={item.id}>
+                  <div className='block  rounded-xl shadow-md border-slate-400 dark:border-slate-600  bg-slate-400 dark:bg-slate-600 h-32 mt-4'>
+                    <div className='flex justify-between'>
+                    <div className='block'>
+                    <div className='p-2 font-semibold font-["font2"] md:text-md text-sm h-20'>{item.title}</div> 
+                      <div className='flex'>
+                        <div className='flex w-32'>
+                          <button className='rounded ml-2 shadow-md bg-slate-200 dark:bg-slate-800 hover:bg-blue-600 hover:text-white dark:hover:bg-blue-600 mr-1' onClick={() => dispatch(incrementQuantity(item.id))}><Plus /></button>
+                          <button className='rounded shadow-md bg-slate-200 dark:bg-slate-800 hover:bg-blue-600 hover:text-white dark:hover:bg-blue-600 mr-1' onClick={() => dispatch(decrementQuantity(item.id))}><Minus /></button>
+                          <button className='rounded shadow-md bg-slate-200 dark:bg-slate-800 hover:bg-blue-600 hover:text-white dark:hover:bg-blue-600' onClick={() => dispatch(removeFromCart(item.id))}><X /></button>   
+                        </div>
+                        <div className='flex'>
+                          <div className='font-["font2"] md:text-md text-sm'>{item.price}</div>
+                          <div><X /></div>
+                          <div className='font-["font2"] md:text-md text-sm'>{item.quantity}</div>
+                          <div><Equal /></div>
+                          <div className='font-["font2"] md:text-md text-sm'>{item.price * item.quantity}</div>
+                        </div>
+                      </div>
+                    </div>
+                    <img src={item.image} alt={item.title} className='w-32 h-32 min-w-32 rounded-r-xl' />
+                    </div>
+                  </div>
                 </div>
             ))}
+            {cart.length > 0 &&
+            <div className='mt-8 text-center w-[50%] ml-[25%] rounded-xl font-semibold font-["font2"] text-white p-2 mb-32 md:text-xl text-lg bg-red-600'>Total: {TotalPrice}</div>}
         </div>
     )
 }
--- src/app/components/redux/cartSlice.js
+++ src/app/components/redux/cartSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import alertify from 'alertifyjs';
+import 'alertifyjs/build/css/alertify.css';
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -6,26 +8,27 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       const itemExists = state.find((item) => item.id === action.payload.id);
-      const pro = state.find((pro) => pro.id === action.payload);
+      //const pro = state.find((pro) => pro.id === action.payload);
 
       if (itemExists) {
         if(itemExists.quantity < 10){
         itemExists.quantity++;
-        //alert('Added to Cart!')
+        alertify.success('Added to Cart!');
         }else{
-        //alert('This Is Maximum Amount You Can Buy!')
+        alertify.error('This Is Maximum Amount You Can Buy!');
         }
       } else {
         state.push({ ...action.payload, quantity: 1 });
-        //alert('Added to Cart!')
+        alertify.success('Added to Cart!');
       }
     },
     incrementQuantity: (state, action) => {
       const item = state.find((item) => item.id === action.payload);
       if(item.quantity === 10){
-        alert('This Is Maximum Amount You Can Buy!')
+        alertify.error('This Is Maximum Amount You Can Buy!');
       }else{
       item.quantity++;
+      alertify.success('Added to Cart!');
       }
     },
     decrementQuantity: (state, action) => {
@@ -33,13 +36,16 @@ const cartSlice = createSlice({
       if (item.quantity === 1) {
         const index = state.findIndex((item) => item.id === action.payload);
         state.splice(index, 1);
+        alertify.error('Remove from Cart!')
       } else {
         item.quantity--;
+        alertify.error('Remove from Cart!')
       }
     },
     removeFromCart: (state, action) => {
       const index = state.findIndex((item) => item.id === action.payload);
       state.splice(index, 1);
+      alertify.error('Remove from Cart!')
     },
   },
 });
